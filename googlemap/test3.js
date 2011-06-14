158cd0bf3edbf380422581d1bc837148c4d69e14
Loïc Raucy <loic.raucy@gmail.com>
2011-06-14 03:26:55 +0200

    Usage of google fusion tables
    We put data on google fusion tables,
    We use these datas, and when we click on a point we display all
    datas from this point

---
+++ googlemap/test3.js
@@ -0,0 +1,52 @@
+var map;
+var countryCircle;
+
+var countryList = {};
+
+google.load('visualization', '1');
+
+function initialize()
+{
+	var lat_init = new google.maps.LatLng(48.861846, 2.35239);
+	var my_map_option = {
+		zoom: 2,
+		center: lat_init,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	map = new google.maps.Map(document.getElementById("map_canvas"), my_map_option);
+
+	var layer = new google.maps.FusionTablesLayer(992564);
+	layer.setMap(map);
+
+	google.maps.event.addListener(layer, 'click', function(e) {
+		$('#status_map').html(e.row['Content'].value);
+
+		var lieu = e.row['Location'].value;
+
+		var query = new google.visualization.Query('http://www.google.com/fusiontables/gvizdata?tq=' + encodeURIComponent("SELECT 'Content' FROM 992564 WHERE Location='" + lieu + "'"));
+		query.send(getLeftData);
+	});
+
+
+	google.maps.event.addListener(map, 'zoom_changed', function() {
+		$('#status_map').html(map.getBounds());
+	});
+
+}
+
+function getLeftData(response){
+	numRows = response.getDataTable().getNumberOfRows();
+	numCols = response.getDataTable().getNumberOfColumns();
+
+	$('#news').html('');
+
+	for (i = 0; i < numRows; i++)
+	{
+		$('#news').after('<div>');
+		for (j = 0; j < numCols; j++) {
+			$('#news').after(response.getDataTable().getValue(i, j) + ', ');
+		}
+		$('#news').after('</div>');
+	}
+}
+	
