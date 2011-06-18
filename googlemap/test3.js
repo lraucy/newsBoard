253bc9f1b39de68a0959c1c93485543447d73340
Loïc Raucy <loic.raucy@gmail.com>
2011-06-18 17:03:19 +0200

    GUI : now we display marker on the map

--- googlemap/test3.js
+++ googlemap/test3.js
@@ -1,5 +1,6 @@
 var map;
 var countryCircle;
+var tableid = 1019598;
 
 var countryList = {};
 
@@ -7,19 +8,17 @@ google.load('visualization', '1');
 
 function initialize()
 {
-	var lat_init = new google.maps.LatLng(48.861846, 2.35239);
+	geocoder = new google.maps.Geocoder();
 	var my_map_option = {
 		zoom: 2,
-		center: lat_init,
+		center: new google.maps.LatLng(48.861846, 2.35239),
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	map = new google.maps.Map(document.getElementById("map_canvas"), my_map_option);
 
-
+	setData();
 	
-	var layer = new google.maps.FusionTablesLayer(992564);
-	layer.setMap(map);
-
+/*	
 	google.maps.event.addListener(layer, 'click', function(e) {
 		$('#status_map').html(e.row['Content'].value);
 
@@ -36,22 +35,37 @@ function initialize()
 
 
 	google.maps.event.addListener(map, 'zoom_changed', function() {
-	});
+	});*/
 
 }
 
-function getLeftData(response){
+function setData() {
+	var query = new google.visualization.Query('http://www.google.com/fusiontables/gvizdata?tq=' + encodeURIComponent("SELECT Title, Date, Latitude, Longitude, url, Description, Picture FROM 1019598"));
+	query.send(getData);
+
+}
+
+function getData(response) {
 	numRows = response.getDataTable().getNumberOfRows();
 	numCols = response.getDataTable().getNumberOfColumns();
-
-
-	for (i = 0; i < numRows; i++)
-	{
-		$('#news,#info_window').append('<div>');
+	
+	for (i = 0; i < numRows; i++) {
+		var row = [];
 		for (j = 0; j < numCols; j++) {
-			$('#news,#info_window').append(response.getDataTable().getValue(i, j) + ', ');
+			row.push(response.getDataTable().getValue(i, j));
 		}
-		$('#news,#info_window').append('</div>');
+		placePoint(row);
 	}
+
 }
 
+function placePoint(row) {
+	coordinate = new google.maps.LatLng(row[2], row[3]);
+	
+	var marker = new google.maps.Marker({
+		map: map,
+		position: coordinate
+	});
+
+
+}
