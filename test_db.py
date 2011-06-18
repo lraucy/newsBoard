ab8e9e19c53cf1d013a9c0ab39fa953e4582fef0
Loïc Raucy <loic.raucy@gmail.com>
2011-06-18 15:26:20 +0200

    We now add news to fusion table from RSS

--- test_db.py
+++ test_db.py
@@ -6,33 +6,48 @@ from fusiontables.sql.sqlbuilder import SQL
 import fusiontables.ftclient
 from fusiontables.fileimport.fileimporter import CSVImporter
 
+import clientlogindata
+
 from yahoo_placemaker.placemaker import placemaker
 
-URL_SOURCE = 'http://news.google.com/news?ned=us&topic=w&output=rss'
+URL_SOURCE = {'http://news.google.com/news?ned=us&topic=w&output=rss',
+        'http://news.google.com/news?ned=us&topic=h&output=rss',
+        'http://news.google.com/news?ned=us&topic=b&output=rss',
+        'http://news.google.com/news?ned=us&topic=t&output=rss',
+        'http://news.google.com/news?ned=us&topic=m&output=rss',
+        'http://news.google.com/news?ned=us&topic=s&output=rss',
+        'http://news.google.com/news?ned=us&topic=e&output=rss',
+        }
 
 import sys, getpass
 
-tableid = 1011402
+tableid = 1019598
 
-username = sys.argv[1]
-password = getpass.getpass("Enter your password: ")
+auth = clientlogindata.ClientLoginData()
 
-token = ClientLogin().authorize(username, password)
+token = ClientLogin().authorize(auth.login, auth.password)
 ft_client = fusiontables.ftclient.ClientLoginFTClient(token)
 
-p = placemaker(URL_SOURCE)
-
-p.process()
-for i in range(len(p.titles)):
-    rowid = int(ft_client.query(SQL().insert(tableid, {'Title':
-        str(p.titles[i]),
-        'Number': str((i+1)),
-        'Location': str(p.places_def[i]),
-        'Date': str(p.dates[i]),
-        'Latitude': str(p.latitudes[i]),
-        'Longitude': str(p.longitudes[i])
-        })).split("\n")[1])
-    print rowid
+for url in URL_SOURCE:
+    p = placemaker(url)
+    p.process()
+    for i in range(len(p.titles)):
+        if ft_client.query(SQL().select(tableid, None,"url='" +\
+            str(p.main_links[i]) + "'")).count('\n')==1 and p.latitudes[i]!=0 and\
+        p.longitudes[i]!=0:
+            rowid = int(ft_client.query(SQL().insert(tableid, {'Title':
+                str(p.titles[i]),
+                'Number': str((i+1)),
+                'Location': str(p.places_def[i]),
+                'Date': str(p.dates[i]),
+                'Latitude': str(p.latitudes[i]),
+                'Longitude': str(p.longitudes[i]),
+                'url': str(p.main_links[i]),
+                'Picture': str(p.pictures[i]),
+                'Country': str(p.countries[i]),
+                'City': str(p.towns[i]),
+                })).split("\n")[1])
+            print rowid
 
 
 
